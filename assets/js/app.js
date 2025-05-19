51f4a3a4c64455966f67e31d0f4e82dbcd74689f
寶達翔 <houtatsu.s@gmail.com>
2025-05-20 05:55:52 +0900

    add

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,13 @@ $(function () {
   $('.o-menu').on('click', function() {
     $(this).toggleClass('is-active');
     $('.menu-link').toggleClass('is-active');
+    $('body').addClass('no_scroll');
   });
 
 
   $('.c-menu').on('click', function() {
     $(this).toggleClass('is-active');
     $('.menu-link').toggleClass('is-active');
+    $('body').removeClass('no_scroll');
   });
 });
